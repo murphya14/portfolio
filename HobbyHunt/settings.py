c622416754d792f878f1b24b34da1c5baeee6a42
murphya14 <aislingmurphy14@hotmail.com>
2020-03-30 19:08:16 +0000

    Fixed the env and manage.py differences

--- HobbyHunt/settings.py
+++ HobbyHunt/settings.py
@@ -18,10 +18,6 @@ if path.exists('env.py'):
     import env 
 
 
-if os.environ.get('DEVELOPMENT'): 
-    development = True
-else:
-    development = False 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,15 +80,7 @@ WSGI_APPLICATION = 'HobbyHunt.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-if development: 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-                    }
-                }
-else:
-     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
