e9225002fdbd54c7f034dd692a3576a873249dd6
murphya14 <aislingmurphy14@hotmail.com>
2020-04-18 12:31:24 +0000

    Fixed auction urls

--- auction/urls.py
+++ auction/urls.py
@@ -1,18 +1,11 @@
+from django.conf.urls import url
+from .views import create, auctions, detail, bid
 
-from django.urls import path
-
-from . import views
 app_name = 'auctions'
 
 urlpatterns = [
-    path('create/', views.create, name='create'),
-    # path('myauctions/', views.my_auctions, name='my_auctions'),
-    # Example: /auctions/
-    path('', views.auctions, name='auctions'),
-    # Example: /auctions/5/
-    path('<int:auction_id>/', views.detail, name='detail'),
-    # Example: /auctions/5/results/
-    # path('<int:auction_id>/results/', views.results, name='results'),
-    # Example: /auctions/5/bid/
-    path('<int:auction_id>/bid/', views.bid, name='bid'),
+    url(r'^create/', create, name='create'),
+    url(r'^auctions/', auctions, name='auctions'),
+    url(r'^detail/', detail, name='detail'),
+    url(r'^bid/(?P<id>\d+)', bid, name='bid'),
 ]
