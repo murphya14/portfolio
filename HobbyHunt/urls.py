1ac54bbbe1bb3f6c935eb9da4aea71f133eb8117
murphya14 <aislingmurphy14@hotmail.com>
2020-04-19 09:46:39 +0000

    Fixed url for auction and changed link in base.html

--- HobbyHunt/urls.py
+++ HobbyHunt/urls.py
@@ -19,10 +19,10 @@ from accounts.views import index, logout, login, registration, user_profile
 from django.views.generic import RedirectView
 from django.views.static import serve
 from .settings import MEDIA_ROOT
-from auction import urls as auction_urls
 from accounts import urls as accounts_urls
 from accounts.views import index
 from hobby_product import urls as urls_hobby_product
+from auction import urls as urls_auction
 from cart import urls as urls_cart
 from hobby_product.views import all_hobby_products
 from search import urls as urls_search
@@ -38,7 +38,7 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
     url(r'^$', index, name="index"),
     url(r'^accounts/', include(accounts_urls)),
-    url(r'^auctions/', include(auctions_urls)),
+    url(r'^auctions/', include(urls_auction)),
     url(r'^products/', include(urls_hobby_product)),
     url(r'^cart/', include(urls_cart)),
     url(r'^checkout/', include(urls_checkout)),
