eb0adcae897744f3b18fc8406e9ce5a5b2468d60
murphya14 <aislingmurphy14@hotmail.com>
2020-04-19 10:41:15 +0000

    Fixed the views.py in hobby_products

--- hobby_product/views.py
+++ hobby_product/views.py
@@ -3,5 +3,5 @@ from .models import hobby_product
 
 # Create your views here.
 def all_hobby_products(request):
-    products = hobby_product.objects.all()
+    hobby_products = hobby_product.objects.all()
     return render(request, "hobby_product.html", {"products": hobby_products})
