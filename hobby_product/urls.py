3ce431328150e98af93018a920e386343d4937cc
murphya14 <aislingmurphy14@hotmail.com>
2020-04-19 10:38:50 +0000

    Added the link to find all products

--- hobby_product/urls.py
+++ hobby_product/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from .views import all_hobby_products
 
 urlpatterns = [
-    url(r'^$', all_hobby_products, name='products'),
+    url(r'^$', all_hobby_products, name='all_hobby_products'),
 ]
