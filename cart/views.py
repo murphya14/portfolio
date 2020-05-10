6b3d1fd092493b0964e76d322b998503a4deef28
murphya14 <aislingmurphy14@hotmail.com>
2020-05-10 11:43:40 +0000

    Changed the views.py in cart to remove the quantity and incorporate the auction

--- cart/views.py
+++ cart/views.py
@@ -1,12 +1,28 @@
 
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+from auction.models import Auction, Bid
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def view_cart(request):
     """A View that renders the cart contents page"""
-    return render(request, "cart.html")
+    user = auth.get_user(request)
+    auctions = Auction.objects.filter(winner=user).filter(paid=False)
+    total = 0 
 
+    for auction in auctions:
+        total += auction.winning_bid
+    
+    context = {
+        'auctions': auctions,
+        'total': total
+    }
+    return render(request, "cart.html", context)
 
+@login_required
 def add_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
     quantity = int(request.POST.get('quantity'))
@@ -17,19 +33,27 @@ def add_to_cart(request, id):
     request.session['cart'] = cart
     return redirect(reverse('index'))
 
-
-def adjust_cart(request, id):
+@login_required
+def adjust_cart(request, auction_id):
     """
     Adjust the quantity of the specified product to the specified
     amount
     """
-    quantity = int(request.POST.get('quantity'))
+    auction = get_object_or_404(Auction, pk=auction_id)
+    user_default = get_object_or_404(User, pk=1)
+
+    auction = request.POST.get('auction.hobby_product')
     cart = request.session.get('cart', {})
 
-    if quantity > 0:
-        cart[id] = quantity
+    if auction:
+        auction.winner = user_default
+        auction.save()
+        messages.success(request, 'Product {0} was removed from the cart'.format(auction.hobby_product))
+        return redirect('view_cart')
     else:
-        cart.pop(id)
+        messages.error(request, 'Sorry we are unable remove this form your cart')
+        return redirect('view_cart')
     
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
