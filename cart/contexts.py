8695ac550775d9872d25d503b6c1574f446eeb6c
murphya14 <aislingmurphy14@hotmail.com>
2020-05-11 18:35:35 +0000

    Fixed cart function by removing hobby_product as an arguemnet

--- cart/contexts.py
+++ cart/contexts.py
@@ -3,7 +3,11 @@ from django.shortcuts import get_object_or_404
 from auction.models import hobby_product, Auction
 
 
-def cart_contents(request, auction):
+from django.shortcuts import get_object_or_404
+from auction.models import hobby_product
+
+
+def cart_contents(request):
     """
     Ensures that the cart contents are available when rendering
     every page
@@ -12,14 +16,11 @@ def cart_contents(request, auction):
 
     cart_items = []
     total = 0
-    hobby_product_count = 0
-    
-    for auction_id in cart.items(auction_id):
-        hobby_product = get_object_or_404(hobby_product, pk=auction_id)
-        total = auction.winning_bid
-      
-        cart_items.append({'id': auction_id, 'product': hobby_product})
-    
-    return {'cart_items': cart_items, 'total': total, 'product_count': hobby_product_count}
 
-
+    
+    for id in cart.items():
+        hobby_product = get_object_or_404(hobby_product, pk=id)
+        total = hobby_product.winning_bid
+        cart_items.append({'id': id, 'product': hobby_product})
+    
+    return {'cart_items': cart_items, 'total': total}
