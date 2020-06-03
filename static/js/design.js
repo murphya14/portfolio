65d8a9da4d3f0802a6d547ee9aa634c048c9af66
murphya14 <aislingmurphy14@hotmail.com>
2020-06-03 14:46:57 +0000

    Connected to mdbootstrap

--- static/js/design.js
+++
@@ -1,6 +0,0 @@
-  console.log("design.js is loaded.")
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
-      
