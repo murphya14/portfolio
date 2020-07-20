e70219575c5cbc5725077898baa603af2f8be54d
murphya14 <aislingmurphy14@hotmail.com>
2020-07-20 10:40:01 +0000

    Removed env.py

--- env.py
+++
@@ -1,5 +0,0 @@
-import os 
-
-os.environ.setdefault("SECRET_KEY", "%6-v+)9sc1gt++ye471$jq+zh)q($712ur*zw(@-e90^2$o9-j")
-
-os.environ.setdefault("DATABASE_URL", "postgres://dncvhebsmgjphq:0da84b8615a38ed1903cf671140979ab6984ae7dbda50f299c0db88d4a030349@ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/db0gi8v2q6bfgg")
