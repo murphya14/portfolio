13da6f9b21026e4c93cffeddd770ea3d5847069c
murphya14 <aislingmurphy14@hotmail.com>
2021-02-13 16:11:31 +0000

    Form is done and formatted

--- home/forms.py
+++ home/forms.py
@@ -4,15 +4,16 @@ from django import forms
 
 # our new form
 class ContactForm(forms.Form):
-    contact_name = forms.CharField(required=True)
-    contact_email = forms.EmailField(required=True)
+    fields = ['contact_name', 'contact_email', 'content']
+    contact_name = forms.CharField(required=True,  widget=forms.TextInput(attrs={'class': 'form-control'})) 
+    contact_email = forms.EmailField(required=True,  widget=forms.TextInput(attrs={'class': 'form-control'}))
     content = forms.CharField(
         required=True,
-        widget=forms.Textarea
+        widget=forms.Textarea(attrs={'class': 'form-control special'})
     )
     
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
         self.fields['contact_name'].label = "Name:"
         self.fields['contact_email'].label = "Email:"
-        self.fields['content'].label = "Message"
+        self.fields['content'].label = "Message:"
