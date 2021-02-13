bbff9669aeaf8f0e93184f2c4447244eb4f91be2
murphya14 <aislingmurphy14@hotmail.com>
2021-02-13 11:33:55 +0000

    Working forms

--- HobbyHunt/settings.py
+++ HobbyHunt/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '(i9b4aes#h1)m3h_8jh^duxrdh$4pu8-q5vkba2yf$ptd1lev_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
