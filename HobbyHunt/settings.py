150596e49cb84b312e7be6e0e4a4083a0a321c77
murphya14 <aislingmurphy14@hotmail.com>
2021-02-27 12:02:07 +0000

    Updated the allowed host

--- HobbyHunt/settings.py
+++ HobbyHunt/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = '(i9b4aes#h1)m3h_8jh^duxrdh$4pu8-q5vkba2yf$ptd1lev_'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['hobbyhunt.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['hobbyhunt.herokuapp.com', 'localhost', ['*']]
 
 
 # Application definition
